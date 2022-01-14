7aa2b0d0eb54a3560e22fa424652734c62a4eefa
Jonathan Hurtig <jonathanhurtig@gmail.com>
2022-01-14 16:33:37 +0100

    fixed typo in .env

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const app = express()
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 
-// connect to mongoDN
-mongoose.connect(process.env.MONGI_URI)
+// connect to mongoDB
+mongoose.connect(process.env.MONGO_URI)
 
 app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }))
