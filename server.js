22179d0fcbb5433ce97fd65795a999b931b3f26e
Jonathan Hurtig <jonathanhurtig@gmail.com>
2022-02-28 17:53:14 +0100

    testing if find can be limited by dates greater than...

--- server.js
+++ server.js
@@ -150,7 +150,7 @@ app.get('/api/users/:_id/logs', async (req, res) => {
   const user = await User.findOne({ _id: userId }) // is needed?
 
   // find all exercises for user in question
-  const logs = await Exercise.find({ userId: userId })
+  const logs = await Exercise.find({ userId: userId, date: { $gte: reqFrom } })
 
   // arrange constructor
   function Log (description, duration, date) {
